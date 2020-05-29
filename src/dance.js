cc16b39f930895d3d5f5f3d8adac8d3691cb8bdd
david vnl <io@davole.com>
2020-05-29 09:28:54 +0200

    Sweep the cave and add even more stuff
    It's almost unbearable how mysterious it was, it has become, and all the
    possibilities that are yet to explore.

--- src/dance.js
+++ src/dance.js
@@ -1,32 +1,60 @@
 // Epic misdirections
-loadJSON(mysterymachine);
+var mysteries = [];
+grabTheMysteries(mysteryMachine);
 
-function loadJSON(callback) {
+function grabTheMysteries(callback) {
   var xhr = new XMLHttpRequest();
   xhr.overrideMimeType("application/json");
   xhr.open('GET', '/src/mysteries.json', true);
   xhr.onreadystatechange = function () {
-    if (xhr.readyState == 4 && xhr.status == "200")
-      callback(JSON.parse(xhr.responseText)[0]);
+    if (xhr.readyState == 4 && xhr.status == "200") {
+      mysteries = JSON.parse(xhr.responseText)[0];
+      callback();
+    }
   };
   xhr.send(null);
 }
 
-function mysterymachine(mysteries) {
-  const array = mysteries.clean;
-  const rnd = Math.floor(Math.random() * array.length);
-  document.getElementById('slogan').textContent = `the ${megalize(array[rnd])}`;
+function mysteryMachine() {
+  var lilHandOfTime = new Date().getHours();
+  var mysteryPool = lilHandOfTime >= 23 && lilHandOfTime <= 5 ? mysteries.questionable : mysteries.clean;
+  var rnd = Math.floor(Math.random() * mysteryPool.length);
+  document.getElementById('slogan').textContent = `the ${accentuate(mysteryPool[rnd])}`;
 }
 
-function megalize(mystery) {
+function accentuate(mystery) {
   return mystery.split(' ').map(x => x.charAt(0).toUpperCase() + x.slice(1)).join(' ');
 }
 
 // Epic sound FX
-var sound = new Howl({
-  src: ['/src/lulululu.wav']
+var lulululus = 0;
+var vibes = new Howl({
+  src: ['/src/lulululu.wav'],
+  onplay: () => {
+    document.body.className = 'focus'
+    isLulululus = true;
+  },
+  onend: () => {
+    document.body.className = '';
+    mysteryMachine();
+  }
 });
 
+function randlulululu() {
+  return Math.floor(Math.random() * 3 + 3);
+}
+
 function lulululu() {
-  sound.play();
+  if (vibes.playing())
+    return;
+
+  lulululus++;
+
+  if (lulululus >= randlulululu()) {
+    vibes.rate(0.5);
+    lulululus = 0;
+  } else
+    vibes.rate(1);
+
+  vibes.play();
 }
